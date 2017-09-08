dcc6dc6256da3bebd2ddaa87d17644525b934206
kenji <gunsecondstage0106@gmail.com>
2017-09-08 15:12:10 +0900

    http.requestやgetは単調な実装になってしまうためsuperagentを使用した。

--- test/keyPostAndGetTest.js
+++ test/keyPostAndGetTest.js
@@ -1,26 +1,20 @@
+const assert = require('assert');
 const Server = require('../lib/Server.js');
 
 (async() => {
   try {
     const server = await Server.create();
 
-    console.log('--- Start Get Test ---');
-    const previousKey = await server.doGetKey();
-    console.log(`  get previous key = ${previousKey} \n`);
-
-    console.log('--- Start Post Test ---');
+    console.log('- Test Start');
     const newKey = 'toNodeKey';
-    const result = await server.doPostKey(newKey);
+    await server.doPostKey(newKey);
+
     const newKeyGetFromServer = await server.doGetKey();
-    // I want to use power assert
-    if(newKeyGetFromServer === newKey) {
-      console.log('  Test Success.');
-    } else {
-      console.log(`  Test Failed.  \n` +
-                  `      POST key value: ${newKey} \n` +
-                  `      GET  key value: ${newKeyGetFromServer}`);
-    }
+    assert.equal(newKey, newKeyGetFromServer, ` Error! "${newKey}" != "${newKeyGetFromServer}" `)
+    console.log('  Success!');
+
   } catch(err) {
+    console.log('  Failed...');
     console.log(err);
   }
 })();
