17d47958c379842e0b33bb419ade3bbb01f58982
kenji <gunsecondstage0106@gmail.com>
2017-09-12 16:19:10 +0900

    テストが通らなかったバグを修正した。

--- test/companiesPostTest.js
+++ test/companiesPostTest.js
@@ -6,18 +6,15 @@ const Company = require('../lib/model/Company.js');
     const server = await Server.create();
 
     const companies = [];
-    for(let i = 1; i <= 2; i++) {
+    for(let i = 1; i <= 10; i++) {
       const company = new Company('ID00' + i, 'model化' + i);
       company.overView('Test概要');
       company.workPlace('Tokyo');
       company.employeeNum(i * 10);
       company.salaryLow(i * 20);
       company.salaryHigh(i * 30);
-      //console.log(JSON.stringify(company));
-      companies.push(JSON.stringify(company));
+      companies.push(company.toJSON());
     }
-    const noInfoCompany = { id: 'ID011', name: '全未入力テスト', overView: '', workPlace: '', employeeNum: 0, salaryLow: 0, salaryHigh: 0 };
-    companies.push(noInfoCompany);
 
     console.log('- Test Start');
     await server.doPostCompanies(companies);
