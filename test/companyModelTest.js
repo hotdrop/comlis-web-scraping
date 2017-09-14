c432cf582be38a2f67780698d8ea4a20e704a169
kenji <gunsecondstage0106@gmail.com>
2017-09-14 11:36:31 +0900

    overViewのテストを作成。 また、一部想定と違う挙動をしたコードを書いていたので修正した。

--- test/companyModelTest.js
+++ test/companyModelTest.js
@@ -3,23 +3,49 @@ const Company = require('../lib/model/Company.js');
 
 function idNullAndEmptyTest() {
   console.log(' ID Null and Empty Test Start');
-  let company = Company.create(null, 'Test1');
-  assert(company === null);
-  company = Company.create('', 'Test2');
-  assert(company === null);
+  assert(Company.create(null, 'Test1') === null);
+  assert(Company.create('', 'Test2') === null);
   console.log(' ID Test Success\n');
 }
 
 function nameNullAndEmptyTest() {
   console.log(' Name Null and Empty Test Start');
-  let company = Company.create('ID001', null);
-  assert(company === null);
-  company = Company.create('ID001', '');
-  assert(company === null);
+  assert(Company.create('ID001', null) === null);
+  assert(Company.create('ID001', '') === null);
   console.log(' Name Test Success\n');
+}
+
+function overViewTest() {
+  console.log(' OverView Test. Run 2 type of test.');
+  const company = Company.create('ID001', 'shield');
+
+  console.log('  [null test start]');
+  company.overView(null);
+  assert(company._overView === '');
+  console.log('  [null test success!]');
+
+  console.log('  [max length test start]');
+  const makeStrByLength = function(length) {
+    let str = '';
+    for (let i = 1; i <= length; i++)
+      str += 'a';
+    return str;
+  };
+
+  const overLenghtStr = makeStrByLength(201);
+  company.overView(overLenghtStr);
+  assert(company._overView.length === 200);
+
+  const maxLengthStr = makeStrByLength(200);
+  company.overView(maxLengthStr);
+  assert(company._overView.length === 200);
+  console.log('  [max length test success!]');
+
+  console.log(' OverView Test Success\n');
 }
 
 console.log('- Test Start');
 idNullAndEmptyTest();
 nameNullAndEmptyTest();
+overViewTest();
 console.log('- Test End');
