34e454eea297472fefc68646aa6e78eb5f7d716c
kenji <gunsecondstage0106@gmail.com>
2017-09-15 12:56:31 +0900

    テストコードの書き方がjavaチックだった為リファクタリング。 JavaScriptらしい書き方が不明のためどうか・・

--- test/companyModelTest.js
+++ test/companyModelTest.js
@@ -1,182 +1,185 @@
 const assert = require('assert');
 const Company = require('../lib/model/Company.js');
 
-function idNullAndEmptyTest() {
-  console.log(' - ID Test Start.');
-  assert(Company.create(null, 'Test1') === null);
-  assert(Company.create('', 'Test2') === null);
-  console.log(' - ID Test Success.\n');
-}
+(() => {
 
-function nameNullAndEmptyTest() {
-  console.log(' - Name Test Start');
-  assert(Company.create('ID001', null) === null);
-  assert(Company.create('ID001', '') === null);
-  console.log(' - Name Test Success.\n');
-}
+  const idNullAndEmptyTest = () => {
+    console.log(' - ID Test Start.');
+    assert(Company.create(null, 'Test1') === null);
+    assert(Company.create('', 'Test2') === null);
+    console.log(' - ID Test Success.\n');
+  };
 
-function overViewTest() {
-  console.log(' - OverView Test start.');
-  const company = Company.create('ID001', 'shield');
+  const nameNullAndEmptyTest = () => {
+    console.log(' - Name Test Start');
+    assert(Company.create('ID001', null) === null);
+    assert(Company.create('ID001', '') === null);
+    console.log(' - Name Test Success.\n');
+  };
 
-  console.log('  - [null] test start.');
-  company.overView = null;
-  assert(company._overView === '');
-  console.log('  - [null] test success!');
+  const overViewTest = () => {
+    console.log(' - OverView Test start.');
+    const company = Company.create('ID001', 'shield');
 
-  console.log('  - [max length] test start.');
-  const overLenghtStr = makeStrByLength(201);
-  company.overView = overLenghtStr;
-  assert(company._overView.length === 200);
-  const maxLengthStr = makeStrByLength(200);
-  company.overView = maxLengthStr;
-  assert(company._overView.length === 200);
-  console.log('  - [max length] test success!');
+    console.log('  - [null] test start.');
+    company.overView = null;
+    assert(company._overView === '');
+    console.log('  - [null] test success!');
 
-  console.log(' - OverView Test Success.\n');
-}
+    console.log('  - [max length] test start.');
+    const overLenghtStr = makeStrByLength(201);
+    company.overView = overLenghtStr;
+    assert(company._overView.length === 200);
+    const maxLengthStr = makeStrByLength(200);
+    company.overView = maxLengthStr;
+    assert(company._overView.length === 200);
+    console.log('  - [max length] test success!');
 
-function workPlaceTest() {
-  console.log(' - WorkPlace Test start.');
-  const company = Company.create('ID002', 'hydra');
+    console.log(' - OverView Test Success.\n');
+  };
 
-  console.log('  - [null] test start.');
-  company.workPlace = null;
-  assert(company._workPlace === '');
-  console.log('  - [null] test success!');
+  const workPlaceTest = () => {
+    console.log(' - WorkPlace Test start.');
+    const company = Company.create('ID002', 'hydra');
 
-  console.log('  - [max length] test start.');
-  const overLengthStr = makeStrByLength(51);
-  company.workPlace = overLengthStr;
-  assert(company._workPlace.length === 50);
-  const maxLengthStr = makeStrByLength(50);
-  company.workPlace = maxLengthStr;
-  assert(company._workPlace.length === 50);
-  console.log('  - [max length] test success!');
+    console.log('  - [null] test start.');
+    company.workPlace = null;
+    assert(company._workPlace === '');
+    console.log('  - [null] test success!');
 
-  console.log(' - WorkPlace Test Success.\n');
-}
+    console.log('  - [max length] test start.');
+    const overLengthStr = makeStrByLength(51);
+    company.workPlace = overLengthStr;
+    assert(company._workPlace.length === 50);
+    const maxLengthStr = makeStrByLength(50);
+    company.workPlace = maxLengthStr;
+    assert(company._workPlace.length === 50);
+    console.log('  - [max length] test success!');
 
-function makeStrByLength(length) {
-  let str = '';
-  for (let i = 1; i <= length; i++)
-    str += 'a';
-  return str;
-}
+    console.log(' - WorkPlace Test Success.\n');
+  };
 
-function employeeNumTest() {
-  console.log(' - EmployeeNum Test start.');
-  const company = Company.create('ID003', 'ATCU');
+  const makeStrByLength = (length => {
+    let str = '';
+    for (let i = 1; i <= length; i++)
+      str += 'a';
+    return str;
+  });
 
-  console.log('  - [null] test start');
-  company.employeeNum = null;
-  assert(company._employeeNum === 0);
-  console.log('  - [null] test success!');
+  const employeeNumTest = () => {
+    console.log(' - EmployeeNum Test start.');
+    const company = Company.create('ID003', 'ATCU');
 
-  console.log('  - [not number] test start.');
-  company.employeeNum = 'aaa';
-  assert(company._employeeNum === 0);
-  console.log('  - [not number] test success!');
+    console.log('  - [null] test start');
+    company.employeeNum = null;
+    assert(company._employeeNum === 0);
+    console.log('  - [null] test success!');
 
-  console.log('  - [range number] test start.');
-  company.employeeNum = -1;
-  assert(company._employeeNum === 0);
-  company.employeeNum = 100000;
-  assert(company._employeeNum === 0);
-  company.employeeNum = 1;
-  assert(company._employeeNum === 1);
-  company.employeeNum = 99999;
-  assert(company._employeeNum === 99999);
-  console.log('  - [range number] test success!');
+    console.log('  - [not number] test start.');
+    company.employeeNum = 'aaa';
+    assert(company._employeeNum === 0);
+    console.log('  - [not number] test success!');
 
-  console.log(' - EmployeeNum Test Success.\n');
-}
+    console.log('  - [range number] test start.');
+    company.employeeNum = -1;
+    assert(company._employeeNum === 0);
+    company.employeeNum = 100000;
+    assert(company._employeeNum === 0);
+    company.employeeNum = 1;
+    assert(company._employeeNum === 1);
+    company.employeeNum = 99999;
+    assert(company._employeeNum === 99999);
+    console.log('  - [range number] test success!');
 
-function salaryLowTest() {
-  console.log(' - SalaryLow Test start.');
-  const company = Company.create('ID004', 'Stark Company');
+    console.log(' - EmployeeNum Test Success.\n');
+  };
 
-  console.log('  - [null] test start.');
-  company.salaryLow = null;
-  assert(company._salaryLow === 0);
-  console.log('  - [null] test success!');
+  const salaryLowTest = () => {
+    console.log(' - SalaryLow Test start.');
+    const company = Company.create('ID004', 'Stark Company');
 
-  console.log('  - [not number] test start.');
-  company.salaryLow = 'aaa';
-  assert(company._salaryLow === 0);
-  console.log('  - [not number] test success!');
+    console.log('  - [null] test start.');
+    company.salaryLow = null;
+    assert(company._salaryLow === 0);
+    console.log('  - [null] test success!');
 
-  console.log('  - [range number] test start.');
-  company.salaryLow = 99;
-  assert(company._salaryLow === 0);
-  company.salaryLow = 10000;
-  assert(company._salaryLow === 0);
-  company.salaryLow = 100;
-  assert(company._salaryLow === 100);
-  company.salaryLow = 9999;
-  assert(company._salaryLow === 9999);
-  console.log('  - [range number] test success!');
+    console.log('  - [not number] test start.');
+    company.salaryLow = 'aaa';
+    assert(company._salaryLow === 0);
+    console.log('  - [not number] test success!');
 
-  console.log(' - SalaryLow Test Success.\n');
-}
+    console.log('  - [range number] test start.');
+    company.salaryLow = 99;
+    assert(company._salaryLow === 0);
+    company.salaryLow = 10000;
+    assert(company._salaryLow === 0);
+    company.salaryLow = 100;
+    assert(company._salaryLow === 100);
+    company.salaryLow = 9999;
+    assert(company._salaryLow === 9999);
+    console.log('  - [range number] test success!');
 
-function salaryHighTest() {
-  console.log(' - SalaryHigh Test start.');
-  const company = Company.create('ID005', 'Avengers');
+    console.log(' - SalaryLow Test Success.\n');
+  };
 
-  console.log('  - [null] test start.');
-  company.salaryHigh = null;
-  assert(company._salaryHigh === 0);
-  console.log('  - [null] test success!');
+  const salaryHighTest = () => {
+    console.log(' - SalaryHigh Test start.');
+    const company = Company.create('ID005', 'Avengers');
 
-  console.log('  - [not number] test start.');
-  company.salaryHigh = 'aaa';
-  assert(company._salaryHigh === 0);
-  console.log('  - [not number] test success!');
+    console.log('  - [null] test start.');
+    company.salaryHigh = null;
+    assert(company._salaryHigh === 0);
+    console.log('  - [null] test success!');
 
-  console.log('  - [range number] test start.');
-  company.salaryHigh = 99;
-  assert(company._salaryHigh === 0);
-  company.salaryHigh = 10000;
-  assert(company._salaryHigh === 0);
-  company.salaryHigh = 100;
-  assert(company._salaryHigh === 100);
-  company.salaryHigh = 9999;
-  assert(company._salaryHigh === 9999);
-  console.log('  - [range number] test success!');
+    console.log('  - [not number] test start.');
+    company.salaryHigh = 'aaa';
+    assert(company._salaryHigh === 0);
+    console.log('  - [not number] test success!');
 
-  console.log(' - SalaryHigh Test Success.\n');
-}
+    console.log('  - [range number] test start.');
+    company.salaryHigh = 99;
+    assert(company._salaryHigh === 0);
+    company.salaryHigh = 10000;
+    assert(company._salaryHigh === 0);
+    company.salaryHigh = 100;
+    assert(company._salaryHigh === 100);
+    company.salaryHigh = 9999;
+    assert(company._salaryHigh === 9999);
+    console.log('  - [range number] test success!');
 
-function toJSONTest() {
-  console.log(' - toJSON Test start.');
-  const resultJson = {id: 'ID006', name: 'nobita manjyuu', overView: 'testes', workPlace: 'NY', employeeNum: 10, salaryLow: 200, salaryHigh: 300};
-  const company = Company.create('ID006', 'nobita manjyuu');
-  company.overView = 'testes';
-  company.workPlace = 'NY';
-  company.employeeNum = 10;
-  company.salaryLow = 200;
-  company.salaryHigh = 300;
-  assert(JSON.stringify(company.toJSON()) === JSON.stringify(resultJson));
-  console.log(' - toJSON Test Success.\n');
-}
+    console.log(' - SalaryHigh Test Success.\n');
+  };
 
-function itemKeyTest() {
-  console.log(' - itemKey Test start.');
-  const company = Company.create('ID007', 'nobi nobita');
-  company.itemKey = 'doraemon';
-  assert(company.itemKey === 'doraemon');
-  console.log(' - itemKey Test Success.\n');
-}
+  const toJSONTest = () => {
+    console.log(' - toJSON Test start.');
+    const resultJson = {id: 'ID006', name: 'nobita manjyuu', overView: 'testes', workPlace: 'NY', employeeNum: 10, salaryLow: 200, salaryHigh: 300};
+    const company = Company.create('ID006', 'nobita manjyuu');
+    company.overView = 'testes';
+    company.workPlace = 'NY';
+    company.employeeNum = 10;
+    company.salaryLow = 200;
+    company.salaryHigh = 300;
+    assert(JSON.stringify(company.toJSON()) === JSON.stringify(resultJson));
+    console.log(' - toJSON Test Success.\n');
+  };
 
-console.log('- Test Start!');
-idNullAndEmptyTest();
-nameNullAndEmptyTest();
-overViewTest();
-workPlaceTest();
-employeeNumTest();
-salaryLowTest();
-salaryHighTest();
-toJSONTest();
-itemKeyTest();
-console.log('- All Test Clear!');
+  const itemKeyTest = () => {
+    console.log(' - itemKey Test start.');
+    const company = Company.create('ID007', 'nobi nobita');
+    company.itemKey = 'doraemon';
+    assert(company.itemKey === 'doraemon');
+    console.log(' - itemKey Test Success.\n');
+  };
+
+  console.log('- Test Start!');
+  idNullAndEmptyTest();
+  nameNullAndEmptyTest();
+  overViewTest();
+  workPlaceTest();
+  employeeNumTest();
+  salaryLowTest();
+  salaryHighTest();
+  toJSONTest();
+  itemKeyTest();
+  console.log('- All Test Clear! \n');
+})();
--- test/lastItemKeyTest.js
+++ test/lastItemKeyTest.js
@@ -1,69 +1,59 @@
 const assert = require('assert');
 
-// テストデータ作成
-const testItems = [];
-testItems.push(new TestItem('001', '2017/09/14 00:00'));
-testItems.push(new TestItem('002', '2017/09/14 05:00'));
-testItems.push(new TestItem('003', '2017/09/14 10:00'));
-testItems.push(new TestItem('004', '2017/09/14 15:00'));
-testItems.push(new TestItem('005', '2017/09/14 20:00'));
-
-let lastItemKey = '';
-
-console.log('- Test Start');
-console.log(` - First Take Item start. LastItemKey = ${lastItemKey}`);
-const firstTakeList = takeItems(lastItemKey);
-for (const item of firstTakeList)
-  console.log(`    ID = ${item.id}  Date = ${item.date}`);
-assert(firstTakeList.length === 5);
-lastItemKey = getLastItemKey(firstTakeList);
-
-testItems.push(new TestItem('006', '2017/09/14 23:00'));
-testItems.push(new TestItem('007', '2017/09/15 05:00'));
-testItems.push(new TestItem('008', '2017/09/15 10:00'));
-
-console.log(` - Second Take Item start. LastItemKey = ${lastItemKey}`);
-const secondTakeList = takeItems(lastItemKey);
-for (const item of secondTakeList)
-  console.log(`    ID = ${item.id}  Date = ${item.date}`);
-assert(secondTakeList.length === 3);
-lastItemKey = getLastItemKey(secondTakeList);
-
-testItems.push(new TestItem('010', '2017/09/15 10:10'));
-
-console.log(` - Third Take Item start. LastItemKey = ${lastItemKey}`);
-const thirdTakeList = takeItems(lastItemKey);
-for (const item of thirdTakeList)
-  console.log(`    ID = ${item.id}  Date = ${item.date}`);
-assert(thirdTakeList.length === 1);
-lastItemKey = getLastItemKey(thirdTakeList);
-
-console.log(` - Fourth Take Item start. LastItemKey = ${lastItemKey}`);
-const fourthTakeList = takeItems(lastItemKey);
-assert(fourthTakeList.length === 0);
-console.log('  Success!');
-
 function TestItem(id, date) {
   this.id = id;
   this.date = date;
 }
 
-function takeItems(lastItemDateEpoch) {
-  const retItems = [];
-  for (const testItem of testItems) {
-    const itemDateEpoch = Date.parse(testItem.date);
-    if (itemDateEpoch > lastItemDateEpoch)
-      retItems.push(testItem);
-  }
-  return retItems;
-}
+(() => {
 
-function getLastItemKey(items) {
-  let lastItemDateEpoch = '';
-  for (const item of items) {
-    const parseDateEpoch = Date.parse(item.date);
-    if (lastItemDateEpoch === '' || parseDateEpoch > lastItemDateEpoch)
-      lastItemDateEpoch = parseDateEpoch;
-  }
-  return lastItemDateEpoch;
-}
+  const executeTest = ((items, lastItemKey, expectedTakeItemNum) => {
+    const getLastItemKey = (items => {
+      let lastItemDateEpoch = '';
+      for (const item of items) {
+        const parseDateEpoch = Date.parse(item.date);
+        if (lastItemDateEpoch === '' || parseDateEpoch > lastItemDateEpoch)
+          lastItemDateEpoch = parseDateEpoch;
+      }
+      return lastItemDateEpoch;
+    });
+    const retItems = [];
+    for (const item of items) {
+      const itemDateEpoch = Date.parse(item.date);
+      if (itemDateEpoch > lastItemKey) {
+        retItems.push(item);
+        console.log(`    ID = ${item.id}  Date = ${item.date}`);
+      }
+    }
+
+    assert(retItems.length === expectedTakeItemNum);
+    return getLastItemKey(items);
+  });
+
+  console.log('- Test Start');
+  const testItems = [];
+  let lastItemKey = '';
+
+  console.log(` - First Take Item start. lastItemKey = ${lastItemKey}`);
+  testItems.push(new TestItem('001', '2017/09/14 00:00'));
+  testItems.push(new TestItem('002', '2017/09/14 05:00'));
+  testItems.push(new TestItem('003', '2017/09/14 10:00'));
+  testItems.push(new TestItem('004', '2017/09/14 15:00'));
+  testItems.push(new TestItem('005', '2017/09/14 20:00'));
+  lastItemKey = executeTest(testItems, lastItemKey, 5);
+
+  console.log(` - Second Take Item start. lastItemKey = ${lastItemKey}`);
+  testItems.push(new TestItem('006', '2017/09/14 23:00'));
+  testItems.push(new TestItem('007', '2017/09/15 05:00'));
+  testItems.push(new TestItem('008', '2017/09/15 10:00'));
+  lastItemKey = executeTest(testItems, lastItemKey, 3);
+
+  console.log(` - Third Take Item start. lastItemKey = ${lastItemKey}`);
+  testItems.push(new TestItem('010', '2017/09/15 10:10'));
+  lastItemKey = executeTest(testItems, lastItemKey, 1);
+
+  console.log(` - Fourth Take Item start. lastItemKey = ${lastItemKey}`);
+  lastItemKey = executeTest(testItems, lastItemKey, 0);
+
+  console.log('- Test Success!');
+})();
