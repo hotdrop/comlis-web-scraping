50f4bc92c9a9520cb9bfb82d3dc8fb751634ac98
kenji <gunsecondstage0106@gmail.com>
2017-09-15 16:14:48 +0900

    導通に成功したため少しだけ整形しREADMEを修正してコミットする。

--- index.js
+++ index.js
@@ -6,20 +6,22 @@ const WebSite = require('./lib/WebSite.js');
   const webSite = await WebSite.create();
 
   try {
-    console.log('- web scraping start! ');
+    console.log('- Web scraping start! ');
     const lastItemKey = await server.doGetKey();
     const companies = await webSite.scraping(lastItemKey);
     if (!companies || companies.length === 0) {
       console.log(' There is no new company information. ');
       return;
     }
+
     const newLastItemKey = webSite.getLastItemKey(companies);
 
     await server.doPostCompanies(companies);
     await server.doPostKey(newLastItemKey);
-    console.log('- web scraping complete. ');
+    console.log('- Web scraping complete. ');
+
   } catch (err) {
-    console.log('- web scraping failed...');
+    console.log('- Web scraping failed...');
     console.log(err);
   }
 })();
