cbc128efa36f89fcd9daafd6a7c4131a128cea2e
kenji <gunsecondstage0106@gmail.com>
2017-09-18 09:57:32 +0900

    Added a note because the explanation was insufficient.

--- env/credentials_sample.js
+++ env/credentials_sample.js
@@ -1,5 +1,6 @@
 module.exports = {
   // Login info of the WebSite to be scraping.
+  // You don't have to setting it unless you need to use it.
   loginname: XXX
   password: XXX
 }
--- env/settings_sample.js
+++ env/settings_sample.js
@@ -1,6 +1,7 @@
 module.exports = {
-  host: 'XXXX',
-  port: 8080,
+  // hostname or IP address and port to connect to the docker container running comlis-data-store application.
+  host: 'XXXX', // example: 192.168.0.1
+  port: XXXX,   // example: 8080
   keyApiEndPoint: '/scraping/lastItemKey',
   companiesApiEndPoint: '/companies'
 }
